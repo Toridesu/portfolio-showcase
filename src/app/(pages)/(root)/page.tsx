7a8a41d4085fc0ae7c6e4c7bae772d461c800693
Toridesu <160250540+Toridesu@users.noreply.github.com>
2025-01-11 15:57:31 +0900

    コンテンツ一覧の実装

--- src/app/(pages)/(root)/page.tsx
+++ src/app/(pages)/(root)/page.tsx
@@ -1,15 +1,26 @@
-import { fetchRepoData } from "@/utils/api";
-import Hero from "./_components/hero"
-import { PROJECTS } from "../../../../_data/contents";
+import Hero from './_components/hero';
+import Gallery from './_components/gallery';
+import { fetchRepoData } from '@/utils/api';
+import { PROJECTS } from '../../../../_data/contents';
 
 export default async function Home() {
-  const res = fetchRepoData(PROJECTS[0].repoUrl);
-  console.log(res)
+  const projects = await Promise.all(
+    PROJECTS.map(async (project) => {
+      const data = await fetchRepoData(project.repoUrl);
+      return {
+        owner: data.owner,
+        slug: data.slug,
+        repoName: data.repoName,
+        topics: data.topics,
+        imageURL: project.imageURL,
+      };
+    })
+  );
+
   return (
-    <main className="space-y-32 pt-16">
-	    {/* <p>ヒーローセクション</p> */}
-      <Hero/>
-	    <p>ギャラリー一覧</p>
+    <main className='space-y-32 pt-16'>
+      <Hero />
+      <Gallery projects={projects.reverse()} />
     </main>
   );
 }
